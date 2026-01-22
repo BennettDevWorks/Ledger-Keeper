f5e5851543228223b6ee95e9fb5dcae4f307ca06
ChristBennett909 <chrisjwbennett@gmail.com>
2026-01-22 01:58:30 +0000

    Adding non functional add section button with hover functionality

--- include/window.hpp
+++ include/window.hpp
@@ -4,8 +4,54 @@
 #include <vector>
 #include <memory>
 #include <string>
+#include "section.hpp"
 #include "raylib.h"
 
+
+class Button{
+	public:
+		Button(int x, int y, const char* title, int width=100, int height=70);
+		~Button();
+
+		//Setters
+		void setWidth(int newWidth);
+		void setHeight(int newHeight);
+		void setTitle(const char* title);
+		void setColor(Color newColor);
+
+		//Getters
+		int getWidth();
+		int getHeight();
+		bool getIsActive();
+		std::string getTitle();
+		Color getColor();
+		Color getActiveColor();
+		Color getStandByeColor();
+		Rectangle getRec();
+
+		//Drawers
+		void drawButton();
+
+		//Actions
+		void action();
+
+	private:
+		int x, y, textX, textY;
+		int fontSize = 12;
+		int width = 100;
+		int height = 70;
+		const char* title;
+		Color color = WHITE;
+		Color standByeColor = WHITE;
+		Color activeColor = DARKGRAY;
+		Color textColor = BLACK;
+		bool isActive;
+};
+
+class AddSectionButton : public Button{
+	public:
+		void drawButton();
+};
 
 class Window{
 	public:
@@ -26,42 +72,14 @@ class Window{
 		void drawWindow();
 		void drawButtons();
 
+		//Update
+		void update();
+
 	private:
 		int width, height;
 		const char* title;
 		std::vector<std::unique_ptr<Button>> buttons;
 		std::vector<std::unique_ptr<Section>> sections;
+		Color bgColor = DARKBLUE;
 };
 
-class Button{
-	public:
-		Button();
-		~Button();
-
-		//Setters
-		void setWidth(int newWidth);
-		void setHeight(int newHeight);
-		void setTitle(const char* title);
-
-		//Getters
-		int getWidth();
-		int getHeight();
-		std::string getTitle();
-
-		//Drawers
-		void drawButton();
-
-		//Actions
-		void action();
-
-	private:
-		int x, y;
-		int width = 100;
-		int height = 70;
-		const char* title;
-};
-
-class AddSectionButton : public Button{
-	public:
-		void drawButton();
-}
--- src/window.cpp
+++ src/window.cpp
@@ -6,10 +6,46 @@
 #include "window.hpp"
 
 
+//------------------Button---------------------
+Button::Button(int x, int y, const char* title, int width, int height)
+: x(x), y(y), width(width), height(height), title(title) {
+	textX = x + (width - MeasureText(title, fontSize)) / 2;
+	textY = y + (height - fontSize) / 2;
+}
+
+Button::~Button(){}
+
+//Setters
+void Button::setWidth(int newWidth){width = newWidth;}
+void Button::setHeight(int newHeight){height = newHeight;}
+void Button::setTitle(const char* newTitle){title = newTitle;}
+void Button::setColor(Color newColor){color = newColor;}
+
+//Getters
+int Button::getWidth(){return width;}
+int Button::getHeight(){return height;}
+bool Button::getIsActive(){return isActive;}
+Color Button::getColor(){return color;}
+Color Button::getActiveColor(){return activeColor;}
+Color Button::getStandByeColor(){return standByeColor;}
+Rectangle Button::getRec(){return Rectangle{(float)x, (float)y, (float)width, (float)height};}
+
+
+//Drawers
+void Button::drawButton(){
+	DrawRectangle(x, y, width, height, color);
+	DrawText(title, textX, textY, fontSize, textColor);
+}
+
+//Actions
+
+//------------------Window--------------------
 Window::Window(int width, int height, const char* title) 
 : width(width), height(height), title(title) {
 	InitWindow(width, height, title);
 	SetTargetFPS(60);
+
+	buttons.push_back(std::make_unique<Button>(10, 10, "Add Section"));
 }
 Window::~Window(){}
 
@@ -33,19 +69,32 @@ std::string Window::getTitle(){return title;}
 void Window::drawWindow(){
 	while (!WindowShouldClose()){
 		BeginDrawing();
-			ClearBackground(DARKBLUE);
+			ClearBackground(bgColor);
+			update();
 			drawButtons();
 		EndDrawing();
 	}
 }
 
 void Window::drawButtons(){
-	for auto& button : buttons{
-		button->draw();
+	for (auto& button : buttons){
+		button->drawButton();
 	}
 }
 
-//-------------------------------Buttons----------------------
+//Update
+void Window::update(){
+	//std::cout << "Updating window" << std::endl;
+	Vector2 mousePos = GetMousePosition();
 
+	for (auto& button : buttons){
+		if (button->getIsActive()){
+			button->setColor(button->getStandByeColor());
+		}
+		if (CheckCollisionPointRec(mousePos, button->getRec())){
+			button->setColor(button->getActiveColor());
+		}
+	}
+}
 
 
